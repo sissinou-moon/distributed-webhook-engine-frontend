be229eff500ecff9ea1d87ed018aef34d494646d
sissinou-moon <sissinouyassine7@gmail.com>
2026-04-26 10:07:25 +0100

    First init

---
+++ pages/auth/sign-in.tsx
@@ -0,0 +1,12 @@
+export default function SignIn() {
+    return (
+        <section className="flex h-screen w-screen items-center justify-center">
+            <div className="">
+
+            </div>
+            <div className="">
+                <h2>Welcome to </h2>
+            </div>
+        </section>
+    );
+}
